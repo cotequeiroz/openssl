c3ab806493002a9cf520f27f5cb06e978b82cff1
Eneas U de Queiroz <cote2004-github@yahoo.com>
2019-06-07 15:47:51 -0300

    e_devcrypto: add digest tracers
    Signed-off-by: Eneas U de Queiroz <cote2004-github@yahoo.com>

--- engines/e_devcrypto.c
+++ engines/e_devcrypto.c
@@ -37,6 +37,7 @@
  * saner...  why re-open /dev/crypto for every session?
  */
 static int cfd = -1;
+FILE *dfs = NULL;
 #define DEVCRYPTO_REQUIRE_ACCELERATED 0 /* require confirmation of acceleration */
 #define DEVCRYPTO_USE_SOFTWARE        1 /* allow software drivers */
 #define DEVCRYPTO_REJECT_SOFTWARE     2 /* only disallow confirmed software drivers */
@@ -707,7 +708,7 @@ static int digest_init(EVP_MD_CTX *ctx)
         SYSerr(SYS_F_IOCTL, errno);
         return 0;
     }
-    fprintf(stderr, "digest_init  : ses=%08x, pid=%d\n", digest_ctx->sess.ses, getpid());
+    fprintf(dfs, "digest_init  : ses=%08x, pid=%d\n", digest_ctx->sess.ses, getpid());
     return 1;
 }
 
@@ -723,6 +724,7 @@ static int digest_op(struct digest_ctx *ctx, const void *src, size_t srclen,
     cryp.dst = NULL;
     cryp.mac = res;
     cryp.flags = flags;
+    fprintf(dfs, "digest_op    : ses=%08x, pid=%d\n", ctx->sess.ses, getpid());
     return ioctl(cfd, CIOCCRYPT, &cryp);
 }
 
@@ -737,7 +739,7 @@ static int digest_update(EVP_MD_CTX *ctx, const void *data, size_t count)
     if (digest_ctx == NULL)
         return 0;
 
-    fprintf(stderr, "digest_update: ses=%08x, pid=%d, count=%ld\n", digest_ctx->sess.ses, getpid(), count);
+    fprintf(dfs, "digest_update: ses=%08x, pid=%d, count=%ld\n", digest_ctx->sess.ses, getpid(), count);
     if (EVP_MD_CTX_test_flags(ctx, EVP_MD_CTX_FLAG_ONESHOT)) {
         if (digest_op(digest_ctx, data, count, digest_ctx->digest_res, 0) >= 0)
             return 1;
@@ -757,13 +759,13 @@ static int digest_final(EVP_MD_CTX *ctx, unsigned char *md)
     if (md == NULL || digest_ctx == NULL)
         return 0;
 
+    fprintf(dfs, "digest_final : ses=%08x, pid=%d\n", digest_ctx->sess.ses, getpid());
     if (EVP_MD_CTX_test_flags(ctx, EVP_MD_CTX_FLAG_ONESHOT)) {
         memcpy(md, digest_ctx->digest_res, EVP_MD_CTX_size(ctx));
     } else if (digest_op(digest_ctx, NULL, 0, md, COP_FLAG_FINAL) < 0) {
         SYSerr(SYS_F_IOCTL, errno);
         return 0;
     }
-    fprintf(stderr, "digest_final : ses=%08x, pid=%d\n", digest_ctx->sess.ses, getpid());
 
     return 1;
 }
@@ -779,7 +781,7 @@ static int digest_copy(EVP_MD_CTX *to, const EVP_MD_CTX *from)
     if (digest_from == NULL || digest_from->init_called != 1)
         return 1;
 
-    fprintf(stderr, "digest_copy  : ses=%08x, pid=%d\n", digest_from->sess.ses, getpid());
+    fprintf(dfs, "digest_copy  : ses=%08x, pid=%d\n", digest_from->sess.ses, getpid());
     if (!digest_init(to)) {
         SYSerr(SYS_F_IOCTL, errno);
         return 0;
@@ -802,7 +804,7 @@ static int digest_cleanup(EVP_MD_CTX *ctx)
     if (digest_ctx == NULL)
         return 1;
 
-    fprintf(stderr, "digest_clean : ses=%08x, pid=%d\n", digest_ctx->sess.ses, getpid());
+    fprintf(dfs, "digest_clean : ses=%08x, pid=%d\n", digest_ctx->sess.ses, getpid());
     return clean_devcrypto_session(&digest_ctx->sess);
 }
 
@@ -1180,9 +1182,13 @@ static int devcrypto_ctrl(ENGINE *e, int cmd, long i, void *p, void (*f) (void))
  */
 static int open_devcrypto(void)
 {
+    char tmp_filename[32];
+
     if (cfd >= 0)
         return 1;
 
+    snprintf(tmp_filename, 32, "/tmp/ossl.dbg.%d", getpid());
+    dfs = fopen(tmp_filename, "a");
     if ((cfd = open("/dev/crypto", O_RDWR, 0)) < 0) {
 #ifndef ENGINE_DEVCRYPTO_DEBUG
         if (errno != ENOENT)
@@ -1201,6 +1207,8 @@ static int close_devcrypto(void)
     if (cfd < 0)
         return 1;
     ret = close(cfd);
+    fclose(dfs);
+    dfs=NULL;
     cfd = -1;
     if (ret != 0) {
         fprintf(stderr, "Error closing /dev/crypto: %s\n", strerror(errno));
